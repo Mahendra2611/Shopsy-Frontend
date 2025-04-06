c965cc9e796cb635a82be0324cb042969fd07852
MAHENDRA PRATAP VERMA <132124880+Mahendra2611@users.noreply.github.com>
2025-04-06 23:08:35 +0530

    Update SocketContext.jsx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketProvider = ({ children }) => {
 
    //const newSocket = io("http://localhost:3000", { withCredentials: true });
 
-   const newSocket = io("https://shopsy-backend-production.up.railway.app", { withCredentials: true });
+   const newSocket = io("https://localmart-backend.onrender.com", { withCredentials: true });
 
 
     newSocket.emit("joinShop", id);
